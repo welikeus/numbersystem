cde732528a13533a3d51ddd93e0fb2b0806f8439
Renat Gaynulin <gaynulinrenat@ngs.ru>
2017-05-14 14:20:40 +0700

    Add CI and start testing

--- test/validation_testsystem.c
+++ test/validation_testsystem.c
@@ -1,12 +1,40 @@
-#include <ctest.h>
 #include <fsys.h>
+#include <ctest.h>
 
-CTEST(fromdecimal, incorrect_data_1)
+CTEST(fromdecimal_suite, incorrect_outsystem_1)
 {
-    
+    const int in = 14;
+    char out[100];
+    const int outsystem = 1;
+    const int result = fromdecimal(in, out, outsystem);
+    const int expected = -1;
+    ASSERT_EQUAL(expected, result);
 }
 
-CTEST(fromdecimal, incorrect_data_2)
+CTEST(fromdecimal_suite, incorrect_outsystem_17)
 {
-    
+    const int in = 27;
+    char out[100];
+    const int outsystem = 17;
+    const int result = fromdecimal(in, out, outsystem);
+    const int expected = -1;
+    ASSERT_EQUAL(expected, result);
+}
+
+CTEST(todecimal_suite, incorrect_insystem_1)
+{
+    char in[] = "00";
+    const int insystem = 1;
+    const int result = todecimal(in, insystem);
+    const int expected = -1;
+    ASSERT_EQUAL(expected, result);
+}
+
+CTEST(todecimal_suite, incorrect_insystem_17)
+{
+    char in[] = "43";
+    const int insystem = 17;
+    const int result = todecimal(in, insystem);
+    const int expected = -1;
+    ASSERT_EQUAL(expected, result);
 }
